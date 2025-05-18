82dcc22bfc4c54abbe75da8fe766728f39c58dd9
@RODRIGUEZMLDO <j.antonio-rm@outlook.com>
2025-05-17 21:19:12 -0600

    actualización de menus

---
+++ general/interfaces/menu_asesor.py
@@ -0,0 +1,16 @@
+def menu_asesor():
+    print("Atención del asesor")
+    print("Operaciones disponibles:")
+    print("1. Consulta de cuentas activas")
+    print("2. Modificación de cuenta")
+    print("3. Alta de nuevas cuentas (captura de datos personales)")
+    print("4. Baja temporal o definitiva")
+    print("0. Salir")
+
+    while True:
+        opcion = input("Seleccione una opción: ")
+        if opcion == "0":
+            print("Saliendo del menú del asesor.")
+            break
+        else:
+            print("")
---
+++ general/interfaces/menu_visitante.py
@@ -0,0 +1,18 @@
+def menu_visitantes():
+    print("Menú para visitantes")
+    print("Operaciones bancarias disponibles:")
+    print("1. Depósitos")
+    print("2. Retiros")
+    print("3. Transferencias")
+    print("4. Pagos de servicios")
+    print("5. Solicitud de creación de cuenta")
+    print("6. Consulta con asesor (Se agrega a la cola FIFO)")
+    print("0. Salir")
+
+    while True:
+        opcion = input("Seleccione una opción: ")
+        if opcion == "0":
+            print("Saliendo del menú de visitantes.")
+            break
+        else:
+            print("")
