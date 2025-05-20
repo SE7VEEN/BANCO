dad1e8799456994d926d720ee6e404527938325f
@RODRIGUEZMLDO <j.antonio-rm@outlook.com>
2025-05-20 02:22:14 -0600

    actualización pcb tercera parte

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ from servidor.PCB_manager import mostrar_pcb
 import shutil, os, json, sys
 from pathlib import Path
 
-BASE_DIR = Path(__file__).parent.parent  # Apunta a BANCO/
+BASE_DIR = Path(__file__).parent  # Apunta a BANCO/
 DATOS_DIR = BASE_DIR / "general" / "datos"
+
 
 def limpiar_y_crear_datos():
     try:
@@ -37,13 +38,9 @@ def limpiar_y_crear_datos():
 
 if __name__ == "__main__":
 
-    
-    """ if not limpiar_y_crear_datos():
+    if not limpiar_y_crear_datos():
         sys.exit(1)  # Salir si hay error
-    print("\n") """
-
-
-    
+    print("\n")
 
     for f in [PCB_PATH, CLIENTES_PATH, CUENTAS_PATH]:
         inicializar_archivo(f)
@@ -101,6 +98,7 @@ if __name__ == "__main__":
             break
         else:
             print("Opción no válida. Intente de nuevo.")
+
 
 
 
