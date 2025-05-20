c30cd5e29d9e3ae1ce0e1f9242d4095c68b52377
@RODRIGUEZMLDO <j.antonio-rm@outlook.com>
2025-05-20 02:57:58 -0600

    actualizando implementaciones

--- Implementaciones/Actualizacion_PCB.py
+++ Implementaciones/Actualizacion_PCB.py
@@ -1,4 +1,13 @@
+from multiprocessing import Process, Lock
+import json, time, os, shutil, sys, datetime
+from pathlib import Path
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from general.utils.utils import PCB_PATH
+
+
 def actualizar_estado_pcb(pid, nuevo_estado):
+    pcb_lock = Lock
     with pcb_lock:
         with open(PCB_PATH, 'r+') as f:
             pcb = json.load(f)
