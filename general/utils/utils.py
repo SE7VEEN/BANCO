e42dc9ef578e8300f62dc6c23e15f02a4705f3aa
Jolu-Santii <huicho-santi-@hotmail.com>
2025-05-19 19:14:48 -0600

    Carpeta datos

--- general/utils/utils.py
+++ general/utils/utils.py
@@ -4,9 +4,18 @@ import json
 import os
 from cliente.cuentas.cuenta2 import Cuenta
 
+import os
 
-CUENTAS_PATH = 'datos/cuentas.json'
+# 1. __file__ apunta a BANCO/general/utils/utils.py
+# 2. Subimos 3 niveles para llegar a BANCO/
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 
+# 3. Construimos ruta hacia BANCO/cliente/datos
+DATOS_DIR = os.path.join(BASE_DIR, 'cliente', 'datos')
+
+# Archivos JSON dentro de cliente/datos
+CUENTAS_PATH = os.path.join(DATOS_DIR, 'cuentas.json')
+CLIENTES_PATH = os.path.join(DATOS_DIR, 'clientes.json')
 
 def quitar_acentos(texto):
     return ''.join(
@@ -31,7 +40,6 @@ def cargar_cuentas():
     return []
 
 def guardar_cuentas(cuentas):
+    os.makedirs(os.path.dirname(CUENTAS_PATH), exist_ok=True) 
     with open(CUENTAS_PATH, 'w') as f:
         json.dump([c.to_dict() for c in cuentas], f, indent=4)
-
-    
