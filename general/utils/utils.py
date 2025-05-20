11c9b6d0269b8d630fcbfcdb6917dddc8f96403e
@RODRIGUEZMLDO <j.antonio-rm@outlook.com>
2025-05-19 18:12:22 -0600

    reorganización de directorios

--- general/utils/utils.py
+++ general/utils/utils.py
@@ -3,6 +3,11 @@ import unicodedata
 import json
 import os
 from cliente.cuentas.cuenta2 import Cuenta
+from servidor.hilos.models import Proceso
+from multiprocessing import Lock
+
+pcb_lock = Lock()
+cuentas_lock = Lock()
 
 
 CUENTAS_PATH = 'datos/cuentas.json'
@@ -34,4 +39,28 @@ def guardar_cuentas(cuentas):
     with open(CUENTAS_PATH, 'w') as f:
         json.dump([c.to_dict() for c in cuentas], f, indent=4)
 
-    
+
+def inicializar_archivo(filename, default=[]):
+    if not os.path.exists(filename):
+        with open(filename, 'w') as f:
+            json.dump(default, f)
+
+def guardar_en_pcb(proceso):
+    with pcb_lock:
+        inicializar_archivo('pcb.json')
+        with open('pcb.json', 'r+') as f:
+            pcb = json.load(f)
+            pcb.append(proceso.to_dict())
+            f.seek(0)
+            json.dump(pcb, f, indent=4)
+
+def obtener_datos_cliente(id_usuario):
+    with cuentas_lock:
+        inicializar_archivo('cuentas.json')
+        with open('cuentas.json', 'r') as f:
+            cuentas = json.load(f)
+        for cuenta in cuentas:
+            if cuenta.get('id_usuario') == id_usuario:
+                return cuenta
+    return None
+
