f8a37f6dfc8bcedb790eaed68601c06a7ebb7e9c
@RODRIGUEZMLDO <j.antonio-rm@outlook.com>
2025-05-19 19:41:54 -0600

    actualizando rutas para pcb

--- general/utils/utils.py
+++ general/utils/utils.py
@@ -10,9 +10,7 @@ pcb_lock = Lock()
 cuentas_lock = Lock()
 
 # Ruta absoluta al archivo pcb.json dentro de banco/general/datos/
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))  # Esto te lleva a 'general'
-DATA_DIR = os.path.join(BASE_DIR, 'Datos')
-PCB_PATH = os.path.join(DATA_DIR, 'pcb.json')
+
 
 import os
 
@@ -26,6 +24,7 @@ DATOS_DIR = os.path.join(BASE_DIR, 'general', 'datos')
 # Archivos JSON dentro de cliente/datos
 CUENTAS_PATH = os.path.join(DATOS_DIR, 'cuentas.json')
 CLIENTES_PATH = os.path.join(DATOS_DIR, 'clientes.json')
+PCB_PATH = os.path.join(DATOS_DIR, 'pcb.json')
 
 def quitar_acentos(texto):
     return ''.join(
@@ -62,20 +61,28 @@ def inicializar_archivo(filename, default=[]):
 
 def guardar_en_pcb(proceso):
     with pcb_lock:
-        inicializar_archivo('pcb.json')
-        with open('pcb.json', 'r+') as f:
-            pcb = json.load(f)
+        inicializar_archivo(PCB_PATH)
+        with open(PCB_PATH, 'r+') as f:
+            try:
+                pcb = json.load(f)
+            except json.JSONDecodeError:
+                pcb = []
             pcb.append(proceso.to_dict())
             f.seek(0)
             json.dump(pcb, f, indent=4)
-    
+            f.truncate()  # Limpia el archivo si el nuevo JSON es más corto que el anterior
+
 def obtener_datos_cliente(id_usuario):
     with cuentas_lock:
-        inicializar_archivo('cuentas.json')
-        with open('cuentas.json', 'r') as f:
-            cuentas = json.load(f)
+        inicializar_archivo(CUENTAS_PATH)
+        with open(CUENTAS_PATH, 'r') as f:
+            try:
+                cuentas = json.load(f)
+            except json.JSONDecodeError:
+                return None
         for cuenta in cuentas:
             if cuenta.get('id_usuario') == id_usuario:
                 return cuenta
     return None
+
 
